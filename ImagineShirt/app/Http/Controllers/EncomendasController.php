72307d09eddb026f5eb7600550cfff0cd8b0e5d3
RicardoBeny <126669644+RicardoBeny@users.noreply.github.com>
2023-06-22 16:39:23 +0100

    PDF css

--- ImagineShirt/app/Http/Controllers/EncomendasController.php
+++ ImagineShirt/app/Http/Controllers/EncomendasController.php
@@ -6,37 +6,47 @@ use Illuminate\Http\Request;
 use Dompdf\Dompdf;
 use Illuminate\Support\Facades\Response;
 use App\Models\Encomendas;
+use Illuminate\Support\Facades\Storage;
 
-
-class EncomendasController extends Controller{
-
-    public function generatePDF($encomenda){
-
+class EncomendasController extends Controller
+{
+    public function generatePDF($encomenda)
+    {
         $encomendaData = self::dadosRecibo($encomenda);
-
         $itemsData = self::itensEncomenda($encomenda);
 
+        $html = view('encomendas.pdf', compact('encomendaData', 'itemsData'))->render();
+
         $dompdf = new Dompdf();
-        $html = view('encomendas.pdf', compact('encomendaData','itemsData'))->render();
+
         $dompdf->loadHtml($html);
+
         $dompdf->setPaper('A4', 'portrait');
+
+        $dompdf->set_option('isRemoteEnabled', true);
+        $dompdf->set_option('isHtml5ParserEnabled', true);
+        $dompdf->set_option('enable_css_float', true);
+        $dompdf->set_option('enable_font_subsetting', true);
+        $dompdf->set_option('enable_javascript', true);
+
         $dompdf->render();
 
         $filename = 'encomenda_' . $encomenda . '.pdf';
 
         $pdfContent = $dompdf->output();
-        file_put_contents(storage_path('app/public/') . $filename, $pdfContent);
 
-        return response()->download(storage_path('app/public/') . $filename)->deleteFileAfterSend(true);
+        $pdfPath = 'pdf_receipts/' . $filename;
+        Storage::put($pdfPath, $pdfContent);
+
+        return response()->download(storage_path('app/' . $pdfPath));
     }
-
     public function showPDF($encomenda){
 
         $encomendaData = self::dadosRecibo($encomenda);
 
         $itemsData = self::itensEncomenda($encomenda);
 
-        return view('encomendas.pdf', compact('encomendaData','itemsData'));
+        return view('encomendas.show', compact('encomendaData','itemsData'));
     }
 
     private function dadosRecibo($encomenda){
@@ -51,7 +61,7 @@ class EncomendasController extends Controller{
 
     private function itensEncomenda($encomenda){
 
-        $itemsData = Encomendas::select('tshirt_images.name','order_items.qty','order_items.size','order_items.unit_price','order_items.sub_total','tshirt_images.url_image','tshirt_images.customer_id')
+        $itemsData = Encomendas::select('tshirt_images.name','order_items.qty','order_items.size','order_items.unit_price','order_items.sub_total','tshirt_images.customer_id')
         ->leftJoin('order_items','orders.id','=','order_items.order_id')
         ->leftJoin('tshirt_images','order_items.tshirt_image_id','=','tshirt_images.id')
         ->where('orders.id', $encomenda)
