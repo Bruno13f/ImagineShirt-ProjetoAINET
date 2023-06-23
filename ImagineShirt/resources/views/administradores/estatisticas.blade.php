2ef84e53036ffc6b7ba7092e612126fb11a332ed
RicardoBeny <126669644+RicardoBeny@users.noreply.github.com>
2023-06-23 03:17:37 +0100

    Estatisticas Update graficos

--- ImagineShirt/resources/views/administradores/estatisticas.blade.php
+++ ImagineShirt/resources/views/administradores/estatisticas.blade.php
@@ -116,104 +116,118 @@
 <script>
 
     // necessario ficar na vista porque usa variaveis dela
-
     document.addEventListener('DOMContentLoaded', function() {
-        var earningsData = {!! json_encode($earningsData) !!};
-        var months = earningsData.map(data => data.month);
-        var earnings = earningsData.map(data => data.earnings);
+    var earningsData = {!! json_encode($earningsData) !!};
+    var months = earningsData.map(data => data.month);
+    var earnings = earningsData.map(data => data.earnings);
 
-        var ctx2 = document.getElementById('earningsChart').getContext('2d');
+    var ctx2 = document.getElementById('earningsChart').getContext('2d');
 
-        new Chart(ctx2, {
-            type: 'line',
-            data: {
-                labels: months,
-                datasets: [{
-                    label: 'Ganhos',
-                    data: earnings,
-                    backgroundColor: 'rgba(0, 123, 255, 0.1)',
-                    borderColor: 'rgba(0, 123, 255, 1)',
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                responsive: true,
-                maintainAspectRatio: true,
-                scales: {
-                    y: {
-                        beginAtZero: true
-                    }
-                },
-                plugins: {
-                    legend: {
+    new Chart(ctx2, {
+        type: 'line',
+        data: {
+            labels: months,
+            datasets: [{
+                label: 'Ganhos',
+                data: earnings,
+                backgroundColor: 'rgba(0, 123, 255, 0.1)',
+                borderColor: 'rgba(0, 123, 255, 1)',
+                borderWidth: 1
+            }]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: true,
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        callback: function(value, index, values) {
+                            return value + ' €';
+                        }
+                    },
+                    gridLines: {
                         display: false
                     }
-                },
-                layout: {
-                    padding: {
-                        top: 10,
-                        bottom: 10,
-                        left: 10,
-                        right: 10
+                }],
+                xAxes: [{
+                    gridLines: {
+                        display: false
                     }
-                }
-            }
-        });
-    });  
-
-    document.addEventListener('DOMContentLoaded', function() {
-        var clientData = {!! json_encode($clientData) !!};
-        var labels = clientData.map(data => data.label);
-        var percentages = clientData.map(data => data.percentage);
-
-        var ctx = document.getElementById('userDistributionChart').getContext('2d');
-
-        new Chart(ctx, {
-            type: 'pie',
-            data: {
-                labels: labels,
-                datasets: [{
-                    data: percentages,
-                    backgroundColor: [
-                        'rgba(75, 192, 192, 0.6)',
-                        'rgba(255, 205, 86, 0.6)',
-                        'rgba(255, 99, 132, 0.6)',
-                    ],
-                    borderColor: [
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(255, 205, 86, 1)',
-                        'rgba(255, 99, 132, 1)',
-                    ],
-                    borderWidth: 1
                 }]
             },
-            options: {
-                responsive: true,
-                maintainAspectRatio: true,
-                plugins: {
-                    legend: {
-                        position: 'right'
-                    },
-                    tooltip: {
-                        callbacks: {
-                            label: function (context) {
-                                var label = context.label || '';
-
-                                if (label) {
-                                    label += ': ';
-                                }
-                                if (context.parsed && context.parsed.toFixed) {
-                                    label += context.parsed.toFixed(2);
-                                }
-                                label += " %";
-                                return label;
-                            }
-                        }
+            legend: {
+                display: false
+            },
+            layout: {
+                padding: {
+                    top: 10,
+                    bottom: 10,
+                    left: 10,
+                    right: 10
+                }
+            },
+            tooltips: {
+                callbacks: {
+                    label: function(tooltipItem, data) {
+                        var value = tooltipItem.yLabel;
+                        return value + ' €';
                     }
                 }
             }
-        });
-    });             
+        }
+    });
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var clientData = {!! json_encode($clientData) !!};
+    var labels = clientData.map(data => data.label);
+    var percentages = clientData.map(data => data.percentage);
+
+    var ctx = document.getElementById('userDistributionChart').getContext('2d');
+
+    new Chart(ctx, {
+        type: 'pie',
+        data: {
+            labels: labels,
+            datasets: [{
+                data: percentages,
+                backgroundColor: [
+                    'rgba(75, 192, 192, 0.6)',
+                    'rgba(255, 205, 86, 0.6)',
+                    'rgba(255, 99, 132, 0.6)',
+                ],
+                borderColor: [
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(255, 205, 86, 1)',
+                    'rgba(255, 99, 132, 1)',
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: true,
+            legend: {
+                position: 'right'
+            },
+            tooltips: {
+                callbacks: {
+                    label: function(tooltipItem, data) {
+                        var dataset = data.datasets[tooltipItem.datasetIndex];
+                        var total = dataset.data.reduce(function(previousValue, currentValue) {
+                            return previousValue + currentValue;
+                        });
+                        var currentValue = dataset.data[tooltipItem.index];;
+                        return data.labels[tooltipItem.index] + ': ' + currentValue + '%';
+                    }
+                }
+            }
+        }
+    });
+});
+
+         
 </script>
 
 @endsection
